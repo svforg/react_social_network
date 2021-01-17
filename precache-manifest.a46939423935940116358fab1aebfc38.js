335a6d301145b1b8a56973737a55f7be12cf838f
svforg <svforgel89@gmail.com>
2021-01-17 22:36:08 +0500

    Updates

---
+++ precache-manifest.a46939423935940116358fab1aebfc38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "109ac8a34cb80726ab47a5acd9e7c777",
+    "url": "/react_social_network/index.html"
+  },
+  {
+    "revision": "a875ed9bb72e7c85708f",
+    "url": "/react_social_network/static/css/main.2f2c89c4.chunk.css"
+  },
+  {
+    "revision": "3f6427160fd0c803d67c",
+    "url": "/react_social_network/static/js/2.6dd2bcaa.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/react_social_network/static/js/2.6dd2bcaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a875ed9bb72e7c85708f",
+    "url": "/react_social_network/static/js/main.d4c1b265.chunk.js"
+  },
+  {
+    "revision": "f2bb6a6f672e910192e6",
+    "url": "/react_social_network/static/js/runtime-main.508f4b08.js"
+  }
+]);
